34f8b2f8df989e5e3095bb83b14ac392d78cf4e7
Sususun <57747099+TonySuSuSun@users.noreply.github.com>
2026-03-26 00:26:29 +0800

    Revert "API Update"
    This reverts commit 1db7ad1d15639eb9a95957c14b4b78caab422e40.

--- app/src/androidTest/java/com/litekite/ime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/litekite/ime/ExampleInstrumentedTest.kt
@@ -18,6 +18,7 @@ package com.litekite.ime
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 
 /**
@@ -27,6 +28,7 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @org.junit.jupiter.api.Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
